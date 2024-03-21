d6304e57fb1659c2985197bf7adb1c0c074846a6
Kunal Kumar <123314137+itz-kunal@users.noreply.github.com>
2024-03-21 12:31:51 +0530

    Update page.jsx

--- frontend/src/app/user/account/page.jsx
+++ frontend/src/app/user/account/page.jsx
@@ -63,7 +63,7 @@ function Page() {
             {/* nav cards */}
             <div className='flex flex-wrap justify-between bg-[#fff] p-2 shadow-md shadow-slate-100'>
                 <div className='border-[2px] border-slate-300 py-1 pl-2 max-w-[43vw] w-[210px] rounded-sm m-1 cursor-pointer' onClick={() => router.push('/user/orders')}>orders</div>
-                <div className='border-[2px] border-slate-300 py-1 pl-2 max-w-[43vw] w-[210px] rounded-sm m-1 cursor-pointer' onClick={() => router.push('/user/coupons')}>coupons</div>
+                <div className='border-[2px] border-slate-300 py-1 pl-2 max-w-[43vw] w-[210px] rounded-sm m-1 cursor-pointer' onClick={() => router.push('/coupons')}>coupons</div>
                 <div className='border-[2px] border-slate-300 py-1 pl-2 max-w-[43vw] w-[210px] rounded-sm m-1 cursor-pointer' onClick={() => router.push('/user/wallet')}>wallet</div>
                 <div className='border-[2px] border-slate-300 py-1 pl-2 max-w-[43vw] w-[210px] rounded-sm m-1 cursor-pointer' onClick={() => router.push('/user/cart')}>carts</div>
             </div>
