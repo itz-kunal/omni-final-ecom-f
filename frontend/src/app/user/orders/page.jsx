bc71e1f32da7e7097cfc7325997ccba8b3e6b7c2
Kunal Kumar <123314137+itz-kunal@users.noreply.github.com>
2024-03-21 11:00:50 +0530

    Update page.jsx

--- frontend/src/app/user/orders/page.jsx
+++ frontend/src/app/user/orders/page.jsx
@@ -8,7 +8,7 @@ import { Link } from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useContext, useEffect, useState } from 'react'
 import { IoCartOutline } from 'react-icons/io5'
-import { checkLogin } from '../../utils/generalFunctions'
+
 import UserContext from '../../context/UseContext'
 import LoadingLayout from '@/components/common/LoadingLayout'
 
